5cb784bb97a2373c1a94282f4ffad25b134741a1
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2025-12-10 21:52:27 +0100

    Actualització main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
+player_x = 2
+player_y = 2
+mode = 0 
 
+def handle_mode_selection():
+
+    global mode
+    
+    if input.button_is_pressed(Button.A):
+        basic.clear_screen()
+        mode = 0
+        return True
+        
+    if input.button_is_pressed(Button.B):
+        basic.clear_screen()
+        mode = 1
+        return True
+        
+    return False
+
+def update_player_position():
+
+    global player_x, player_y
+    
+    gx = input.acceleration(Dimension.X)
+    gy = input.acceleration(Dimension.Y)
+    
+
+    if gx < -150 and player_x > 0:
+        player_x -= 1
+    elif gx > 150 and player_x < 4:
+        player_x += 1
+    
+
+    if gy < -150 and player_y > 0:
+        player_y -= 1
+    elif gy > 150 and player_y < 4:
+        player_y += 1
+
+def render_player_mode():
+ 
+    led.plot(player_x, player_y)
+    basic.pause(50)
+    led.unplot(player_x, player_y)
+
+def render_temperature_mode():
+
+    led.plot_bar_graph(input.temperature(), 50)
+
+while True:
+
+    handle_mode_selection()
+    
+    if mode == 1:
+        render_temperature_mode()
+    else:
+        update_player_position()
+        render_player_mode()
--- main.ts
+++ main.ts
@@ -1,1 +1,58 @@
+let player_x = 2
+let player_y = 2
+let mode = 0
+function handle_mode_selection(): boolean {
+    
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        mode = 0
+        return true
+    }
+    
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        mode = 1
+        return true
+    }
+    
+    return false
+}
 
+function update_player_position() {
+    
+    let gx = input.acceleration(Dimension.X)
+    let gy = input.acceleration(Dimension.Y)
+    if (gx < -150 && player_x > 0) {
+        player_x -= 1
+    } else if (gx > 150 && player_x < 4) {
+        player_x += 1
+    }
+    
+    if (gy < -150 && player_y > 0) {
+        player_y -= 1
+    } else if (gy > 150 && player_y < 4) {
+        player_y += 1
+    }
+    
+}
+
+function render_player_mode() {
+    led.plot(player_x, player_y)
+    basic.pause(50)
+    led.unplot(player_x, player_y)
+}
+
+function render_temperature_mode() {
+    led.plotBarGraph(input.temperature(), 50)
+}
+
+while (true) {
+    handle_mode_selection()
+    if (mode == 1) {
+        render_temperature_mode()
+    } else {
+        update_player_position()
+        render_player_mode()
+    }
+    
+}
